13f86d19df92176d25a0eae4ee41bd8263def991
Povshy <vladcarrib@gmail.com>
2024-03-04 08:20:35 +0200

    444

--- src/container/login/index.js
+++ src/container/login/index.js
@@ -10,9 +10,6 @@ class SignupForm extends Form {
   FIELD_NAME = {
     EMAIL: 'email',
     PASSWORD: 'password',
-    PASSWORD_AGAIN: 'passwordAgain',
-    ROLE: 'role',
-    IS_CONFIRM: 'isConfirm',
   }
   FIELD_ERROR = {
     IS_EMPTY: 'Введіть значення в поле',
@@ -20,10 +17,6 @@ class SignupForm extends Form {
     EMAIL: 'Введіть коректне значення e-mail адреси',
     PASSWORD:
       'Пароль повинен складатися не менше ніж 8 символів, включаючи хоча б одну цифру, малу та велику літеру',
-    PASSWORD_AGAIN:
-      'Ваш другий пароль не збігається з першим',
-    NOT_CONFIRM: 'Ви не погоджуєтеся з правилами',
-    ROLE: 'Ви не обрали роль',
   }
   // ---------------------
 
@@ -47,27 +40,6 @@ class SignupForm extends Form {
         return this.FIELD_ERROR.PASSWORD
       }
     }
-
-    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
-      if (
-        String(value) !==
-        this.value[this.FIELD_NAME.PASSWORD]
-      ) {
-        return this.FIELD_ERROR.PASSWORD_AGAIN
-      }
-    }
-
-    if (name === this.FIELD_NAME.ROLE) {
-      if (isNaN(value)) {
-        return this.FIELD_ERROR.ROLE
-      }
-    }
-
-    if (name === this.FIELD_NAME.IS_CONFIRM) {
-      if (Boolean(value) !== true) {
-        return this.FIELD_ERROR.NOT_CONFIRM
-      }
-    }
   }
   // ------------
 
@@ -80,7 +52,7 @@ class SignupForm extends Form {
       this.setAlert('progress', 'Завантаження...')
 
       try {
-        const res = await fetch('/signup', {
+        const res = await fetch('/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -109,10 +81,14 @@ class SignupForm extends Form {
         this.value[this.FIELD_NAME.EMAIL],
       [this.FIELD_NAME.PASSWORD]:
         this.value[this.FIELD_NAME.PASSWORD],
-      [this.FIELD_NAME.ROLE]:
-        this.value[this.FIELD_NAME.ROLE],
     })
   }
 }
 
 window.signupForm = new SignupForm()
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.session) {
+    location.assign('/')
+  }
+})
